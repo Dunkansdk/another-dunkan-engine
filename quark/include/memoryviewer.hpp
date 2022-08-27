1ac37f791b800a0903f009de5067c635fa34561e
Dunkansdk <emanueldurso1@gmail.com>
2022-08-28 01:02:11 +0200

    Memory viewer, array example

--- quark/include/memoryviewer.hpp
+++ quark/include/memoryviewer.hpp
@@ -1,1 +1,46 @@
 #pragma once
+
+#include <array>
+#include <cstdio>
+#include <cstdint>
+
+namespace QUARK {
+
+	struct MemoryViewer {
+
+		public:
+			constexpr static std::size_t default_width {16};
+
+			static std::size_t calculate_lines(std::size_t const size, std::size_t const width = default_width)
+			{
+    		auto const lines {size / width};
+    		if ((size & 0xF) != 0)
+      		return lines + 1;
+    		return lines;
+			}
+
+			static void show_memory_line(std::uint8_t const* memory, std::size_t width = default_width)
+			{
+				std::printf("%16p ||", (void*)memory);
+				for(std::size_t i{}; i < width; ++i) {
+					std::printf(" %02X", memory[i]);
+				}
+				std::printf("\n");
+			}
+
+			static void show_memory(std::uint8_t const* memory, std::size_t const size, std::size_t width = default_width)
+			{
+				auto const lines { calculate_lines(size, width) };
+				std::printf("     ADDRESS     || 01 00 00 00 02 00 00 00 04 00 00 00 08 00 00 00\n");
+				std::printf("-----------------||------------------------------------------------\n");
+				for(std::size_t i{}; i < lines; ++i) {
+					show_memory_line(memory, width);
+					memory += width;
+				}
+				std::printf("-------------------------------------------------------------------\n");
+			}
+
+		private:
+	};
+
+}
--- quark/include/slotmap.hpp
+++ quark/include/slotmap.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <cstdint>
 
 namespace QUARK {
 
@@ -8,15 +9,22 @@ namespace QUARK {
 	struct Slotmap {
 
 	public:
-		using value_type  = DataType;
+		using value_type 	= DataType;
+		using index_type 	= std::uint32_t;
+		using gen_type	 	= index_type;
+		using key_type		= struct { index_type id; gen_type generation; };
 
 		// constexpr = this should be able to be computed at compile time
 		[[nodiscard]] constexpr std::size_t size() 			const { return m_size; }
 		[[nodiscard]] constexpr std::size_t capacity() 	const { return Capacity; }
 
 	private:
-		std::size_t m_size{};
-		std::array<value_type, Capacity> m_data{};
+		index_type 												m_size{};
+		index_type												m_freelist{};
+		gen_type													m_generation{};
+		std::array<key_type, Capacity> 		m_index{};
+		std::array<value_type, Capacity> 	m_data{};
+		std::array<index_type, Capacity> 	m_erase{};
 
 	};
 
