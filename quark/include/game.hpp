a89060740518be7d235823b058c036866a1b753f
Dunkansdk <emanueldurso1@gmail.com>
2022-08-30 18:56:35 +0200

    Metafunction for loop the TYPES

--- quark/include/game.hpp
+++ quark/include/game.hpp
@@ -4,18 +4,37 @@
 namespace Quark {
 
     namespace METACPP {
+        /*
+         * constant
+         */
         template<typename T, T VALUE>
         struct constant { static constexpr T value { VALUE }; };
-
         struct false_type : constant<bool, false> {};
         struct true_type : constant<bool, true> {};
 
+        /*
+         * is_same_v
+         */
         template<typename T, typename U>
         struct is_same : false_type {};
         template<typename T>
         struct is_same<T, T> : true_type {};
         template<typename T, typename U>
         constexpr bool is_same_v = is_same<T, U>::value;
+
+        /*
+         * nth_type
+         */
+        template<typename T>
+        struct type_id { using type = T; };
+        template<std::size_t N, typename... TYPES>
+        struct nth_type { static_assert(sizeof...(TYPES) != 0, "ERROR: Typelist with 0 types."); };
+        template<std::size_t N, typename... TYPES>
+        using nth_type_t = typename nth_type<N, TYPES...>::type;
+        template<typename T, typename... TYPES>
+        struct nth_type<0, T, TYPES...> : type_id<T> {};
+        template<std::size_t N, typename T, typename... TYPES>
+        struct nth_type<N, T, TYPES...> : type_id<nth_type_t<N - 1, TYPES...>> {};
 
         template<typename... TYPES> // Types = TEnemy, TPlayer, TBullet
         struct Typelist {
@@ -30,12 +49,13 @@ namespace Quark {
 
             template<typename T>
                 consteval static std::size_t pos() noexcept {
-                   // for(std::size_t i{}; i < size(); ++i) {
-                   //     using TypeName = nth_type<i, TYPES...>::type;
-                   //     if(is_same_v<T, TypeName>)
-                   //         return i;
-                   // }
-                    return 1;
+                    static_assert(contains<T>());
+                    /*for(std::size_t i{}; i < size() - 1; ++i) {
+                        using TypeName = nth_type_t<i, TYPES...>;
+                        if(is_same_v<T, TypeName>)
+                            return i;
+                    }*/
+                    return size() - 1;
                 }
         };
     }
@@ -47,7 +67,7 @@ namespace Quark {
         template<typename TAG>
         consteval static std::size_t id()   noexcept {
             static_assert(TAG_LIST::template contains<TAG>());
-            return 1;
+            return TAG_LIST::template pos<TAG>();
         }
 
         template<typename TAG>
--- quark/src/main.cpp
+++ quark/src/main.cpp
@@ -16,29 +16,25 @@ using TagTypes = METACPP::Typelist<TPlayer, TEnemy, TBullet>;
 // Macro
 void seetype(auto) { std::cout << __PRETTY_FUNCTION__ << "\n"; }
 
-template<typename T>
-struct S {
-    static constexpr bool value { true };
-};
+template<typename T, T VALUE>
+struct constant { static constexpr T value { VALUE }; };
 
-template<typename T>
-struct type_id { using type = T; };
-
-template<std::size_t N, typename... TYPES>
-struct nth_type { static_assert(sizeof...(TYPES) != 0, "ERROR: Typelist with 0 types."); };
-template<std::size_t N, typename... TYPES>
-using nth_type_t = typename nth_type<N, TYPES...>::type;
 template<typename T, typename... TYPES>
-struct nth_type<0, T, TYPES...> : type_id<T> {};
-template<std::size_t N, typename T, typename... TYPES>
-struct nth_type<N, T, TYPES...> : type_id<nth_type_t<N - 1, TYPES...>> {};
+struct pos_type;
+template<typename T, typename... TYPES>
+struct pos_type<T, T, TYPES...> {
+    constexpr static std::size_t value { 0 };
+};
+template<typename T, typename U, typename... TYPES>
+struct pos_type<T, U, TYPES...> {
+    constexpr static std::size_t value { 1 + pos_type<T, TYPES...>::value };
+};
 
 int main() {
 
-    seetype(nth_type<0, char, int>{});
-    seetype(nth_type<0, char, int, float>::type{});
-    seetype(nth_type<1, char, int, float>::type{});
-    seetype(nth_type<2, char, int, float>::type{});
+    std::cout << pos_type<int, int, void, float>::value << "\n";
+    std::cout << pos_type<void, int, void, float>::value << "\n";
+    std::cout << pos_type<float, int, void, float>::value << "\n";
 
     return 1;
 }
